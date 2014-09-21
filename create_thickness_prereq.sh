8c36d3b310434445e343b1fd123f715736317eea
Jan Scholz <scholz_sfo+github@hotmail.com>
2014-09-21 18:10:01 -0400

    update ./create_thickness_prereq.sh

--- create_thickness_prereq.sh
+++ create_thickness_prereq.sh
@@ -70,17 +70,42 @@ mincreshape -quiet -clobber -dimorder zspace,yspace,xspace ${TBASE}_potential_ri
 rm ${TBASE}_thickness_labels_{left,right}.mnc
 
 ###############################################################################
+# manual intervention
+
+cp ${OUTBASE}_potential_left{,_orig}.mnc
+cp ${OUTBASE}_potential_right{,_orig}.mnc
+
+echo "change the following files if necessary, then hit any key (ctrl-c to abort)"
+echo "${OUTBASE}_potential_left.mnc ${OUTBASE}_potential_right.mnc"
+read
+
+###############################################################################
 # marching cubes
+
+echo "using standard marching cubes"
+echo "it might be better to remove spurious voxels with the following, though"
+echo "for h in left right; do neighbours.py -v -n 3 --lowerthreshold 3.5 --upperthreshold 7 NRXN1a_version_1_average_votedlabels_potential_\${h}.mnc && marching_cubes NRXN1a_version_1_average_votedlabels_potential_\${h}_connectivity_order3.mnc NRXN1a_version_1_average_votedlabels_cortex_o3thr4k_\${h}.obj 4000; done"
+
+marching_cubes  ${OUTBASE}_potential_left.mnc  ${OUTBASE}_cortex_left.mc.obj  3.5 7
+marching_cubes  ${OUTBASE}_potential_right.mnc ${OUTBASE}_cortex_right.mc.obj 3.5 7
+
+###############################################################################
+# surface construction
+
+bicobj2iv ${OUTBASE}_cortex_left.mc.obj  > ${OUTBASE}_cortex_left.mc.iv
+bicobj2iv ${OUTBASE}_cortex_right.mc.obj > ${OUTBASE}_cortex_right.mc.iv
+
+echo "now simplify/smooth in amira"
+echo
+echo "then modify iv before converting back to obj"
+echo "/micehome/matthijs/scripts/convert_open-inventor-V8.0-to-V2.1.pl  ${OUTBASE}_cortex_left_smooth_v8.iv  ${OUTBASE}_cortex_left.smooth.iv -clobber"
+echo "/micehome/matthijs/scripts/convert_open-inventor-V8.0-to-V2.1.pl ${OUTBASE}_cortex_right_smooth_v8.iv ${OUTBASE}_cortex_right.smooth.iv -clobber"
+echo
+echo "then convert to obj"
+echo "iv2bicobj  ${OUTBASE}_cortex_left.smooth.{iv,obj}"
+echo "iv2bicobj ${OUTBASE}_cortex_right.smooth.{iv,obj}"
+
 
 #minccalc -quiet -clobber -expression "A[1]>0 ? A[0] : 0"  ${OUTBASE}_potential.mnc  ${OUTBASE}_cortex_left.mnc  ${OUTBASE}_potential_left.mnc
 #minccalc -quiet -clobber -expression "A[1]>0 ? A[0] : 0"  ${OUTBASE}_potential.mnc  ${OUTBASE}_cortex_right.mnc  ${OUTBASE}_potential_right.mnc
-
-marching_cubes  ${OUTBASE}_potential_left.mnc  ${OUTBASE}_cortex_left.obj  3.5 7
-marching_cubes  ${OUTBASE}_potential_right.mnc ${OUTBASE}_cortex_right.obj 3.5 7
-
-bicobj2iv ${OUTBASE}_cortex_left.obj  > ${OUTBASE}_cortex_left.iv
-bicobj2iv ${OUTBASE}_cortex_right.obj > ${OUTBASE}_cortex_right.iv
-
-echo "now simplify/smooth in amira"
-
 
