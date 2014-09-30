e145845a20eb17f844b9b9d9ea4170bdf465f805
Jan Scholz <scholz_sfo+github@hotmail.com>
2014-09-30 14:25:23 -0400

    update ./micescripts/runModel.R

--- micescripts/runModel.R
+++ micescripts/runModel.R
@@ -1,5 +1,6 @@
-runmodel <- function(table,mask,form,outbase,columns=NULL,random='',anova=FALSE, verbose=F) {
+runmodel <- function(table,mask,form,outbase,columns=NA,random='',anova=FALSE, adjust='fdr', verbose=F) {
 # formula and random are strings
+# write all columns except F-statistic if columns==NA
 	library(RMINC)
 
 	if (!'filename' %in% colnames(table)) warning('missing column: filename')
@@ -23,23 +24,38 @@ runmodel <- function(table,mask,form,outbase,columns=NULL,random='',anova=FALSE,
 		attach(table)
 		cat('running mincLme \n')
 		out <- mincLme(filename, form, random, mask=mask)
+		detach(table)
 	}
 
-	q <- mincFDR(out, mask=mask, method="FDR")
+	if (adjust %in% c("holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr")) {
+		# lme outpout has no df and statType fields
+		#q <- mincFDR(out, mask=mask, method="FDR", statType=c('F','t','t','t','t'), df=rep(10,5))
+		q <- mincFDR(out, mask=mask, method="FDR")
+	}
+
 	colnames <- dimnames(out)[[2]]
 
-	if (!is.null(columns)) {
-		selectedcolumns <- columns
-	} else {
+	if (is.null(columns)) {
+		selectedcolumns <- c()
+	} else if (is.na(columns)) {
 		start <- 1
 		if (colnames[start] == 'F-statistic') start <- 2
 		selectedcolumns <- start:length(colnames)
+	} else {
+		for (x in columns) {
+			if(!is.numeric(x) | x>length(colnames)) {
+				stop('not a valid column:',x)
+			}
+		}
+		selectedcolumns <- columns
 	}
 
 	for (c in selectedcolumns) {
 		outname <- paste(outbase,gsub('[()]','',colnames[c]),sep='_')
 		mincWriteVolume(out, paste(outname,".mnc",sep=""),     c,clobber=T)
-		mincWriteVolume(  q, paste(outname,"_fdr.mnc",sep=""), c,clobber=T)
+		if (adjust %in% c("holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr")) {
+			mincWriteVolume(  q, paste(outname,"_fdr.mnc",sep=""), c,clobber=T)
+		}
 		cat('saved', outname, '\n')
 	}
 
