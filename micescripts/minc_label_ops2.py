bd04220d3f388d2fbea261e79ff8a8e20029a1d2
Jan Scholz <scholz_sfo+github@hotmail.com>
2014-10-03 14:28:02 -0400

    update ./micescripts/minc_label_ops2.py

--- micescripts/minc_label_ops2.py
+++ micescripts/minc_label_ops2.py
@@ -25,6 +25,7 @@ if __name__ == "__main__":
     parser.add_option("--remove", dest="remove",
                       help="Label numbers to remove (comma separated, i.e. 3,7,211) - outfile will exclude these labels",
                       type="string")
+    #parser.add_option("--clean", help="convert to round integers", action="store_true", default=False)
     parser.add_option("--binarize", help="threshold >0.5 and binarize output", action="store_true", default=False)
     parser.add_option("-v", "--verbose", help='verbose output', action='store_true', default=False)
 
@@ -32,7 +33,7 @@ if __name__ == "__main__":
 
     if len(args) != 2:
         parser.error("Incorrect number of arguments")
-    
+
 
     inlabels = volumeFromFile(args[0], dtype='ubyte')
     outlabels = volumeLikeFile(args[0], args[1])
@@ -41,6 +42,7 @@ if __name__ == "__main__":
         print("Labels found in input: %s" % unique(inlabels.data[::]))
 
     if options.remap:
+        print 'v', options.clean
         outlabels.data[::] = inlabels.data[::]
         labelpairs = options.remap.split(',')
         for pair in labelpairs:
@@ -48,7 +50,7 @@ if __name__ == "__main__":
             if options.verbose:
                 print("Mapping: %d to %d" % (int(labels[0]), int(labels[1])))
             outlabels.data[inlabels.data == int(labels[0])] = int(labels[1])
-    
+
     if options.select:
         labels = options.select.split(',')
         for l in labels:
@@ -72,7 +74,15 @@ if __name__ == "__main__":
 
     if options.binarize:
         outlabels.data[outlabels.data > 0.5] = 1
-                      
+
+#    if options.clean:
+#        outlabels.data[::] = inlabels.data[::]
+#        #print('foo')
+#        #for l in range(min(inlabels.data[::]),max(inlabels.data[::])+1):
+#        #   if options.verbose:
+#        #       print "Mapping: %d" % l
+#        #   outlabels.data[outlabels.data > 0.5] = 1
+
     # write to file
     outlabels.writeFile()
     outlabels.closeVolume()
