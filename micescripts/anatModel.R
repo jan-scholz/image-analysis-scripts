86f2053d45af5e460288cf3b7957ea2191c26d35
Jan Scholz <scholz_sfo+github@hotmail.com>
2014-11-23 11:26:37 -0500

    update ./micescripts/anatModel.R

--- micescripts/anatModel.R
+++ micescripts/anatModel.R
@@ -1,4 +1,4 @@
-anatModel <- function(data,formula,atlas,anova=F,combined=F,adjust='fdr') {
+anatModel <- function(data,formula,atlas,anova=F,combined=F,relative=F,adjust='fdr') {
 	# anatModel() retrieves volumes for each atlas label and returns the p-value
 	#             for the model specified by the formula
 	#
@@ -29,27 +29,38 @@ anatModel <- function(data,formula,atlas,anova=F,combined=F,adjust='fdr') {
 		vols <- anatCombineStructures(vols, method=method)
 	}
 
+	if (relative) {
+		vols <- vols / rowSums(vols) * 100
+	} else {
+		vols <- cbind(vols,brain=rowSums(vols))
+	}
+
+	vols <- as.data.frame(vols)           # removes meta information: $atlas, $anatIDs
+
 	data <- melt(cbind(data, vols), id=seq(ncol(data)))
 	labelnames <- levels(data$variable)
+	data$hemisphere <- sapply(data$variable, function(x) if (grepl('^left',x)) 'left' else if (grepl('^right',x)) 'right' else 'combined')
+	data$structure <-  sapply(data$variable,  function(x) gsub('^(left|right) ','',x))
+	data$variable <- NULL
 
 	# correct formula if wrong left side
 	formula <- formula(gsub('^.*~ *','value ~ ', deparse(formula)))
 
 	# initalize output array
 	if (anova) {
-		effectnames <- rownames(anova(lm(formula, data=subset(data, variable==labelnames[1]))))
+		effectnames <- rownames(anova(lm(formula, data=subset(data, structure==labelnames[1]))))
 	} else {
-		effectnames <- names(coef(lm(formula, data=subset(data, variable==labelnames[1]))))
+		effectnames <- names(coef(lm(formula, data=subset(data, structure==labelnames[1]))))
 	}
 	p <- array(NA, dim=c(ncol(vols), length(effectnames)), dimnames=list(labelnames,effectnames))
 
 	# populate array with p values
 	for (s in rownames(p)) {
 		if (anova) {
-			p[s,] <- anova(lm(formula, data=subset(data,variable==s)))$`Pr(>F)`
+			p[s,] <- anova(lm(formula, data=subset(data,structure==s)))$`Pr(>F)`
 		} else {
-			#c[s] <- coef(lm(formula, data=subset(data,variable==s)))
-			p[s,] <- coef(summary(lm(formula, data=subset(data,variable==s))))[,'Pr(>|t|)']
+			#c[s] <- coef(lm(formula, data=subset(data,structure==s)))
+			p[s,] <- coef(summary(lm(formula, data=subset(data,structure==s))))[,'Pr(>|t|)']
 		}
 	}
 
@@ -62,10 +73,23 @@ anatModel <- function(data,formula,atlas,anova=F,combined=F,adjust='fdr') {
 		adjust <- 'none'
 	}
 
+	attr(p, "data") <- data
+	attr(p, "relative.volumes") <- relative
 	attr(p, "p.adjust.method") <- adjust
 	attr(p, "formula") <- deparse(formula)
 	attr(p, "anova") <- anova
 
 	return(p)
 }
+
+
+# assign class 'foo' to object
+#
+# print.foo <- function(x,...) {
+# attributes(x) <- NULL
+# print(x)
+# }
+# d <- attr(out,'data')
+# d <- attr(out,'data')
+# ggplot(d,aes(scan,value,colour=condition)) + stat_smooth() + geom_point() + facet_wrap(~structure, scales='free') + opts(title='absolute')
 
